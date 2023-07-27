b7517e86b8a67c1846ddf7a84fd639477dd332b0
alonso.md <alonso2295@hotmail.com>
2023-07-26 21:58:50 -0500

    Use of default_args

--- dags/default_args.py
+++ dags/default_args.py
@@ -4,25 +4,33 @@ from airflow.operators.bash import BashOperator
 
 from datetime import datetime, timedelta
 
+
+default_args = {
+    'owner': 'Squad A',
+    'depends_on_past': True,
+    'execution_timeout': timedelta(seconds=10),
+    'retries': 3,
+    'retry_delay': timedelta(seconds=30)
+}
+
+
 with DAG(dag_id="default_args",
          start_date=datetime(2023,7,24),
          catchup=False,
-         schedule='@daily' #schedule and schedule_interval are available, but schedule_interval is going to be deprecated.
+         schedule='@daily', #schedule and schedule_interval are available, but schedule_interval is going to be deprecated.
+         default_args=default_args
         ) as dag:
 
     extract = EmptyOperator(task_id="extract", 
-                            owner="Squad_A",
-                            depends_on_past=True)
+                            )
 
     transform = BashOperator(task_id="transform",
-                             bash_command="sleep 50",
-                             owner="Squad_A",
-                             execution_timeout=timedelta(seconds=30))
+                             bash_command="sleep 20",
+                             execution_timeout=timedelta(seconds=30)
+                            )
 
     load = BashOperator(task_id="load",
-                        bash_command="exit 1",
-                        owner="Squad_B",
-                        retries=3,
-                        retry_delay=timedelta(seconds=30))
+                        bash_command="exit 1"
+                        )
 
     extract >> transform >> load
